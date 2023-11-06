eb4ef9f4a6e0aad6238067460c31e148ebb5c8fb
jstone122100 <jstone122100@gmail.com>
2023-11-05 21:17:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Example JavaScript functionality
+document.addEventListener('DOMContentLoaded', function () {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', function () {
+        // Add your logic for what happens when the CTA button is clicked
+        alert('Let\'s meditate!');
+    });
+});
